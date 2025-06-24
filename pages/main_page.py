7bc2c596aff8b03975c9acb6cfb7d139dd416ffd
kalyaminat <kalyaminat@yandex.ru>
2025-06-24 20:51:46 +0300

    "Группировка, подготовка к ревью"

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,9 +1,6 @@
 from .base_page import BasePage
-from .locators import BasketPageLocators
-from .login_page import LoginPage
-from selenium.common.exceptions import NoAlertPresentException
-from selenium.webdriver.common.by import By
-from .product_page import ProductPage
+
+
 
 
 class MainPage(BasePage):
